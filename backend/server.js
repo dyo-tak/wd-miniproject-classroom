6fa347762b00842819ef76a143ead70aa63b8839
Dyotak <dyotak10@gmail.com>
2023-10-15 23:21:54 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,37 @@
 require('dotenv').config()
 
-const express = require('express')
-const mongoose = require('mongoose')
-const noteRoutes = require('./routes/notes')
-const userRoutes = require('./routes/user')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import the cors middleware
+const noteRoutes = require('./routes/notes');
+const userRoutes = require('./routes/user');
 
 // express app
-const app = express()
+const app = express();
+
+// Enable CORS for all routes (This allows all origins, which can be adjusted as needed)
+app.use(cors());
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/notes', noteRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/notes', noteRoutes);
+app.use('/api/user', userRoutes);
 
-// connect to db
+// connect to the database
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('Connected to the database & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.error(error);
+  });
